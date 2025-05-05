cb8b8d17d5aaee09113fcd5b9cf458d449ef9e8e
Aarushi <aarushimandloi744@gmail.com>
2025-05-05 23:14:31 +0530

    fifth_commit

--- scripts.js
+++ scripts.js
@@ -2,3 +2,7 @@ const navDialog = document.getElementById('nav-dialog');
 function handleMenu() {
     navDialog.classList.toggle('hidden');
 }
+
+function setupsectionObserver(element,isLTF,speed){
+    const intersectionObserver = new IntersectionObserver();
+}
