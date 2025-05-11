5e7c0f504722ee25b0f1003c7277df2914d06396
Aarushi <aarushimandloi744@gmail.com>
2025-05-11 23:28:33 +0530

    All_fix_on_campamyLogo_animation

--- scripts.js
+++ scripts.js
@@ -1,59 +1,16 @@
-// const navDialog = document.getElementById('nav-dialog');
-// function handleMenu() {
-//     navDialog.classList.toggle('hidden');
-// }
-
-// const intialtranslateLTR = -48*4;
-// const intialtranslateRTL = 36*4;
-
-// function setupsectionObserver(element,isLTR,speed){
-
-//     const intersectionCallback = (entries) =>{
-//         const isIntersecting = entries[0].isIntersecting;
-//         if(isIntersecting){
-//             document.addEventListener('scroll',scrollHandler);
-//         }
-//         else{
-//             document.removeEventListener('scroll',scrollHandler);
-//            } 
-//         }
-// const intersectionObserver = new IntersectionObserver(intersectionCallback); 
-
-// intersectionObserver.observe(element);
-
-// function scrollHandler(){
-//     const translateX = (window.innerHeight - element.getBoundingClientRect().top )* speed;
-
-//   let totalTranslate = 0;
-//   if(isLTR){
-//     totalTranslate = translateX + intialtranslateLTR;
-//   }  else{
-//     totalTranslate = -(translateX + intialtranslateRTL);
-//   }
-//   element.style.transform = `translateX(${totalTranslate}px)`;
-//  }
-//     }
-
-//     const line1 = document.getElementById('line1');
-//     const line2 = document.getElementById('line2');
-//     const line3 = document.getElementById('line3');
-
-// setupsectionObserver(line1,true,0.15);
-// setupsectionObserver(line2,false,0.15);
-// setupsectionObserver(line3,true,0.15);  
-
-
 const navDialog = document.getElementById('nav-dialog');
 
 function handleMenu() {
     navDialog.classList.toggle('hidden');
 }
 
-const initialTranslateLTR = -48 * 4;
-const initialTranslateRTL = 36 * 4;
+const initialTranslateLTR = 0;
+const initialTranslateRTL = 0; 
 
 function setupSectionObserver(element, isLTR, speed) {
-    let scrollHandler;
+    // let scrollHandler;
+        let scrollHandler = null;
+
     const intersectionCallback = (entries) => {
         const isIntersecting = entries[0].isIntersecting;
         if (isIntersecting) {
